e04677c1dd4e6c27258e483a1e12df43dae25f83
Eli-Debusk <elidebusk.edu@gmail.com>
2023-12-27 11:21:16 -0800

    update Joystick2d.java

--- src/main/java/frc/rosemont/util/Joystick2d.java
+++ src/main/java/frc/rosemont/util/Joystick2d.java
@@ -1,14 +1,34 @@
 package frc.rosemont.util;
 
+import java.util.function.Supplier;
+
 public class Joystick2d {
     
-    private double y;
-    private double x;
+    private Supplier<Double> y;
+    private Supplier<Double> x;
     private boolean reverseY = false;
 
     public Joystick2d() {}
 
     public Joystick2d(boolean reverseY) {
         this.reverseY = reverseY;
+    }
+
+    public void set(Supplier<Double>[] xyaxes) {
+        this.x = xyaxes[0];
+        this.y = xyaxes[1];
+    }
+
+    public double getMagnitude() {
+        return Math.sqrt(
+            Math.pow(x.get(), 2) + Math.pow(reverseY == true ? -y.get() : y.get(), 2)
+        );
+    }
+
+    public double[] getAxes() {
+        return new double[] {
+            x.get(),
+            reverseY == true ? -y.get() : y.get()
+        };
     }
 }
